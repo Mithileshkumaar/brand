9d8c85190ea87b0b9383e49ced5ae8fdce3cd60d
Mithileshkumaar <mithilesh.kumaar@yahoo.com>
2022-01-17 22:04:03 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="nav">
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top " style="background-color: rgb(26, 25, 25);">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">APPLE</a>
+    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style=" background-color:rgb(46, 44, 44)" >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><router-link to="/">Home</router-link></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">    <router-link to="/about">About</router-link></a>
+        </li>
+        
+          
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+<ul class="nav justify-content-end" style="padding-top:50px">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+
+    <!--  |
+ -->
+  </div>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+ background-color: black;
+
+ 
+}
+
+#nav {
+ 
+background-color: rgb(26, 25, 25);
+
+}
+
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #fff;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="about " style=" background-color:black;">
+    
+     <div class="card  text-white" style="background-color:black; letter-spacing:4px; height:800px;">
+  <img src="../assets/b.jpg" class="card-img" alt="..." style="height:600px; width:100%">
+  <div class="card-img-overlay" style="padding-top:500px; ">
+    <h5 class="card-title h1">APPLE EVENT</h5>
+    <p class="card-text" style=" text-transform: uppercase; padding-top:10px">please join us for an APPLE special event at </p> <p>THE STEVE JOBS THEATER</p>
+    <p class="card-text">AND</p>
+    <p class="card-text"> WATCH IT LIVE ONLY ON APPLE VIDEOS</p>
+    </div>
+    </div>
+    
+   <div class="card wrapper"  >
+  <div class="card-body" style="">
+    <div class="container">
+
+<div class="row g-5 gt-3 pt-3" style="margin-left:100px">
+  <div class="col-6 col-sm-4" >
+     <div class="flip-card" >
+  <div class="flip-card-inner" style="border-radius:30px;">
+    <div class="flip-card-front">
+      <img src="../assets/i.jpg" alt="Avatar ">
+    </div>
+    <div class="flip-card-back pt-5" >
+      <h1>IPHONE 16</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+
+  </div>
+  <div class="col-6 col-sm-4" >
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="../assets/w.jpg" alt="Avatar" >
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="col-6 col-sm-4">
+        <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="../assets/p.jpg" alt="Avatar" >
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+    
+    
+  </div>
+  <div class="col- col-sm-4" >
+        <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="../assets/t.jpg" alt="Avatar" >
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+  </div>
+   <div class="col- col-sm-4" >
+        <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="../assets/h.jpg" alt="Avatar" >
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+  </div>
+  <div class="col-6 col-sm-4">
+        <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="../assets/tv.jpg" alt="Avatar" >
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+  </div>
+</div>
+    </div>
+</div>
+   </div>
+    
+  </div>
+</template>
+<style scoped>
+.wrapper {
+  position: relative;
+  width: auto;
+  height: auto;
+  color: #FFFFFF;
+  
+  background: linear-gradient(#80FFDB, #5390D9);;
+}
+.flip-card {
+  background-color: transparent;
+  width: 250px;
+  height: 300px;
+  
+  
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: gray;
+  color: white;
+  transform: rotateY(180deg);
+}
+img{
+  height: 300px;
+  width: 250px;
+}
+
+</style>
